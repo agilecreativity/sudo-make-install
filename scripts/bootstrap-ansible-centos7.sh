61672fb47f3c1987b56fc2fa0a50a75a4d3be1f8
agilecreativity <agilecreativity@gmail.com>
2016-10-29 21:58:09 -0400

    Add python2-dnf to ansible script

--- scripts/bootstrap-ansible-centos7.sh
+++ scripts/bootstrap-ansible-centos7.sh
@@ -3,5 +3,7 @@
 sudo yum -y update
 sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 sudo yum -y update
+# Required if you are using Fedora/CentOS and the new dnf module 
+sudo yum install python2-dnf
 sudo yum install ansible
 ansible --version
