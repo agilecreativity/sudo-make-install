0030e692a5ee4213be9b591989be4ce1bc7ceef6
Burin Choomnuan <agilecreativity@gmail.com>
2017-06-27 16:38:46 -0400

    Add workaround script to install ruby 2.3.x on Arch

---
+++ scripts/rbenv-ruby-2-3-4-arch-linux.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+## from: https://github.com/rbenv/ruby-build/issues/1092
+## Note: you may need to first $yaourt gcc5
+CC=/usr/bin/gcc-5 \
+PKG_CONFIG_PATH=/usr/lib/openssl-1.0/pkgconfig \
+rbenv install 2.3.4
